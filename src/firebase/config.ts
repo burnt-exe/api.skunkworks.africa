986d9995da6f18e5a2d2d29ac8c2c4db234254be
Raydo Matthee (Skunkworks) <raydo@skunkworks.africa>
2025-10-08 21:20:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "easydocs-jyi3p",
+  "appId": "1:696496779948:web:4eb9391f815212dd38a509",
+  "apiKey": "AIzaSyCY9vJl86v-2T4bTuGO2vhz2z2tufYsJWU",
+  "authDomain": "easydocs-jyi3p.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "696496779948"
+};
