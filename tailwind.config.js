4ad52dca4ff5cd015b8c74ba66883a7e5a684de0
Raydo Matthee (Skunkworks) <raydo@skunkworks.africa>
2025-10-13 04:00:16 +0000

    Error: ./src/app/globals.css Error evaluating Node.js code CssSyntaxErro

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,html}", // adjust to match your structure
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [],
+};
