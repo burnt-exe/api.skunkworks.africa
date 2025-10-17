85563adba3c60db5badaab6cbf19722a618fdba8
Raydo Matthee (Skunkworks) <raydo@skunkworks.africa>
2025-10-17 09:46:17 +0000

    Try fixing this error: `Runtime Error: Body exceeded 1 MB limit. To conf

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverActions: {
-    bodySizeLimit: '5mb',
+    bodySizeLimit: '10mb',
   },
   typescript: {
     ignoreBuildErrors: true,
